249319f1ec1da20f3fa57c45c2617c08bb0952e7
kirill <polferov@live.com>
2024-09-11 15:27:15 +0200

    changes

--- call-me-maybe/src/main/java/org/example/callmemaybe/CallController.java
+++ call-me-maybe/src/main/java/org/example/callmemaybe/CallController.java
@@ -1,9 +1,7 @@
 package org.example.callmemaybe;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +13,8 @@ public class CallController {
     }
 
     @GetMapping("/call/{depth}")
-    public void printDepth(@PathVariable("depth") int depth, @RequestHeader HttpHeaders headers) {
+    public void printDepth(@PathVariable("depth") int depth) {
         System.out.println("Depth: " + depth);
-        headers.forEach((key, value) -> System.out.println(key + ":" + value));
         if (depth > 0) {
             callService.call(depth - 1);
         }
